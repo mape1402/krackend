53cb4fb8a47e5748e2ef40fc7f1307c51c0569e5
Mario Perez <mperez@elysiumcoding.com>
2026-08-10 14:12:36 -0600

    feat: migrate sagas pigeon messaging adapter

---
+++ src/Krackend.Sagas.Orchestrations.Messaging.Pigeon/MessagingPigeonMarker.cs
@@ -0,0 +1,6 @@
+namespace Krackend.Sagas.Orchestrations.Messaging.Pigeon;
+
+/// <summary>
+/// Marker type for the Krackend.Sagas.Orchestrations.Messaging.Pigeon package.
+/// </summary>
+public sealed class MessagingPigeonMarker;
