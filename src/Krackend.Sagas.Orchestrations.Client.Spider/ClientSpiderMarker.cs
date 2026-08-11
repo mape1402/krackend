70f87c142bc80583d1b8d448f801d577926b9aff
Mario Perez <mperez@elysiumcoding.com>
2026-08-11 16:53:22 -0600

    feat: add spider orchestration pipeline extensions

---
+++ src/Krackend.Sagas.Orchestrations.Client.Spider/ClientSpiderMarker.cs
@@ -0,0 +1,8 @@
+namespace Krackend.Sagas.Orchestrations.Client.Spider;
+
+/// <summary>
+/// Marker type for the Spider orchestration client package.
+/// </summary>
+public sealed class ClientSpiderMarker
+{
+}
