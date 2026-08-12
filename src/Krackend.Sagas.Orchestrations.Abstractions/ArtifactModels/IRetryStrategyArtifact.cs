5f24d86c01c88590757ca88a7e84c5a5064b1a47
Mario Perez <mperez@elysiumcoding.com>
2026-08-12 14:09:30 -0600

    Characterize orchestration artifact runtime contract

--- src/Krackend.Sagas.Orchestrations.Abstractions/ArtifactModels/IRetryStrategyArtifact.cs
+++ src/Krackend.Sagas.Orchestrations.Abstractions/ArtifactModels/IRetryStrategyArtifact.cs
@@ -1,10 +1,13 @@
 namespace Krackend.Sagas.Orchestrations.Abstractions.Artifacts;
 
+using System.Text.Json.Serialization;
 using Krackend.Sagas.Orchestrations.Abstractions.Primitives;
 
 /// <summary>
 /// Represents an immutable retry-strategy contract.
 /// </summary>
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "$artifactType")]
+[JsonDerivedType(typeof(FixedRetryStrategyArtifact), "fixed")]
 public interface IRetryStrategyArtifact
 {
     /// <summary>
--- src/Krackend.Sagas.Orchestrations.Abstractions/ArtifactModels/ITimeoutBehaviorPolicyArtifact.cs
+++ src/Krackend.Sagas.Orchestrations.Abstractions/ArtifactModels/ITimeoutBehaviorPolicyArtifact.cs
@@ -1,10 +1,15 @@
 namespace Krackend.Sagas.Orchestrations.Abstractions.Artifacts;
 
+using System.Text.Json.Serialization;
 using Krackend.Sagas.Orchestrations.Abstractions.Primitives;
 
 /// <summary>
 /// Represents an immutable timeout-behavior policy contract.
 /// </summary>
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "$artifactType")]
+[JsonDerivedType(typeof(FailTimeoutBehaviorPolicyArtifact), "fail")]
+[JsonDerivedType(typeof(WaitTimeoutBehaviorPolicyArtifact), "wait")]
+[JsonDerivedType(typeof(ReconcileTimeoutBehaviorPolicyArtifact), "reconcile")]
 public interface ITimeoutBehaviorPolicyArtifact
 {
     /// <summary>
