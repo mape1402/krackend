b50c2cac1189fa0172a83d2c4d72f89bf9fa1f57
Mario Perez <mperez@elysiumcoding.com>
2026-08-14 15:59:02 -0600

    feat(runtime): host mule actions in runtime storage context

--- tests/Krackend.Sagas.Orchestrations.Tests/EntityFrameworkCore/SqlServerRegistrationTests.cs
+++ tests/Krackend.Sagas.Orchestrations.Tests/EntityFrameworkCore/SqlServerRegistrationTests.cs
@@ -1,6 +1,9 @@
 using Krackend.Sagas.Orchestrations.Abstractions.Runtime.Storage;
 using Krackend.Sagas.Orchestrations.EntityFrameworkCore.SqlServer;
+using Krackend.Sagas.Orchestrations.EntityFrameworkCore.SqlServer.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Mule;
 
 namespace Krackend.Sagas.Orchestrations.Tests.EntityFrameworkCore;
 
@@ -19,5 +22,21 @@ public sealed class SqlServerRegistrationTests
         Assert.Contains(services, descriptor => descriptor.ServiceType == typeof(ITaskDispatchRepository));
         Assert.Contains(services, descriptor => descriptor.ServiceType == typeof(ICompensationExecutionRepository));
         Assert.Contains(services, descriptor => descriptor.ServiceType == typeof(IExecutionTransitionRepository));
+    }
+
+    [Fact]
+    public void RuntimeStorageCanHostMuleDurableActions()
+    {
+        var services = new ServiceCollection();
+
+        services.AddKrackendSagasOrchestrationsSqlServer(options =>
+            options.UseSqlServer("Server=(local);Database=KrackendTests;Trusted_Connection=True;TrustServerCertificate=True"));
+        services.AddMule(mule => mule.UseKrackendSagasOrchestrationsRuntimeStorage());
+
+        using var provider = services.BuildServiceProvider();
+        using var scope = provider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<RuntimeStorageDbContext>();
+
+        Assert.NotNull(dbContext.Model.FindEntityType(typeof(DurableAction)));
     }
 }
