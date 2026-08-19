896691a43ba11bdd525184a8328da759ed62f2d1
Mario Perez <mperez@elysiumcoding.com>
2026-08-19 16:52:04 -0600

    Remove bullshit

--- src/Krackend.Sagas.Orchestrations.Client.Abstractions/ClientAbstractionsMarker.cs
+++
@@ -1,8 +0,0 @@
-namespace Krackend.Sagas.Orchestrations.Client.Abstractions;
-
-/// <summary>
-/// Marker type for the client abstractions package.
-/// </summary>
-public sealed class ClientAbstractionsMarker
-{
-}
--- src/Krackend.Sagas.Orchestrations.Client.Spider/ClientSpiderMarker.cs
+++
@@ -1,8 +0,0 @@
-namespace Krackend.Sagas.Orchestrations.Client.Spider;
-
-/// <summary>
-/// Marker type for the Spider orchestration client package.
-/// </summary>
-public sealed class ClientSpiderMarker
-{
-}
