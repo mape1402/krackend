62f079e42d0d1191474d04b8dadaf723a5ca1d07
Mario Perez <mperez@elysiumcoding.com>
2026-08-19 21:26:13 -0600

    Configure runtime sample with RabbitMQ Pigeon

--- samples/Krackend.Sagas.Orchestrations.RuntimeHost.Sample/Krackend.Sagas.Orchestrations.RuntimeHost.Sample/Program.cs
+++ samples/Krackend.Sagas.Orchestrations.RuntimeHost.Sample/Krackend.Sagas.Orchestrations.RuntimeHost.Sample/Program.cs
@@ -1,14 +1,29 @@
 using Krackend.Sagas.Orchestrations.Runtime.Buffering.Mule;
 using Krackend.Sagas.Orchestrations.Runtime.DependencyInjection;
 using Krackend.Sagas.Orchestrations.Runtime.Messaging.Pigeon;
+using Pigeon.Messaging.Rabbit;
 
 var builder = WebApplication.CreateBuilder(args);
+var rabbitConnectionString = builder.Configuration.GetConnectionString("RabbitMq")
+    ?? "amqp://guest:guest@localhost:5672";
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services
     .AddKrackendOrchestrationsRuntime()
-    .AddPigeon(builder.Configuration)
+    .AddPigeon(builder.Configuration, pigeon =>
+    {
+        pigeon.UseRabbitMq(rabbit =>
+        {
+            rabbit.Url = rabbitConnectionString;
+        });
+        pigeon.ConfigureConsumerExecution(execution =>
+        {
+            execution.MaxConcurrency = null;
+            execution.QueueCapacity = null;
+            execution.PrefetchCount = null;
+        });
+    })
     .AddMule(_ => { });
 
 var app = builder.Build();
