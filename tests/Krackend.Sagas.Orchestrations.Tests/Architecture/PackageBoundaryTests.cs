3dfd4e292c95b8c6912e1898086702b0e941a217
Mario Perez <mperez@elysiumcoding.com>
2026-09-10 04:23:47 -0600

    Add orchestration adapter boundary tests

--- tests/Krackend.Sagas.Orchestrations.Tests/Architecture/PackageBoundaryTests.cs
+++ tests/Krackend.Sagas.Orchestrations.Tests/Architecture/PackageBoundaryTests.cs
@@ -1,6 +1,12 @@
 using System.Reflection;
+using AtlasSchemaRegistryServices = Krackend.Sagas.Orchestrations.SchemaRegistry.Atlas.DependencyInjection.ServiceCollectionExtensions;
+using ClientPigeonServices = Krackend.Sagas.Orchestrations.Client.Messaging.Pigeon.ServiceCollectionExtensions;
 using ClientSpiderServices = Spider.Pipelines.Core.OrchestrationPipelineBuilderExtensions;
 using MuleRuntimeServices = Krackend.Sagas.Orchestrations.Runtime.Buffering.Mule.ServiceCollectionExtensions;
+using RuntimeButterMorphServices = Krackend.Sagas.Orchestrations.Runtime.ButterMorph.DependencyInjection.ServiceCollectionExtensions;
+using RuntimeGossipRedisServices = Krackend.Sagas.Orchestrations.Runtime.Gossip.Redis.ServiceCollectionExtensions;
+using RuntimePigeonServices = Krackend.Sagas.Orchestrations.Runtime.Messaging.Pigeon.ServiceCollectionExtensions;
+using SchemaRegistryServices = Krackend.Sagas.Orchestrations.SchemaRegistry.DependencyInjection.ServiceCollectionExtensions;
 
 namespace Krackend.Sagas.Orchestrations.Tests.Architecture;
 
@@ -40,8 +46,14 @@ public sealed class PackageBoundaryTests
         var referencedAssemblies = Assembly.GetExecutingAssembly()
             .GetReferencedAssemblies()
             .Select(assembly => assembly.Name)
+            .Append(typeof(AtlasSchemaRegistryServices).Assembly.GetName().Name)
+            .Append(typeof(ClientPigeonServices).Assembly.GetName().Name)
             .Append(typeof(ClientSpiderServices).Assembly.GetName().Name)
             .Append(typeof(MuleRuntimeServices).Assembly.GetName().Name)
+            .Append(typeof(RuntimeButterMorphServices).Assembly.GetName().Name)
+            .Append(typeof(RuntimeGossipRedisServices).Assembly.GetName().Name)
+            .Append(typeof(RuntimePigeonServices).Assembly.GetName().Name)
+            .Append(typeof(SchemaRegistryServices).Assembly.GetName().Name)
             .Where(name => name is not null && name.StartsWith("Krackend.Sagas.Orchestrations", StringComparison.Ordinal))
             .ToHashSet(StringComparer.Ordinal);
 
@@ -49,6 +61,7 @@ public sealed class PackageBoundaryTests
         {
             "Krackend.Sagas.Orchestrations.Abstractions",
             "Krackend.Sagas.Orchestrations.Client",
+            "Krackend.Sagas.Orchestrations.Client.Messaging.Pigeon",
             "Krackend.Sagas.Orchestrations.Client.Spider",
             "Krackend.Sagas.Orchestrations.Contracts",
             "Krackend.Sagas.Orchestrations.ControlPlane",
@@ -57,8 +70,13 @@ public sealed class PackageBoundaryTests
             "Krackend.Sagas.Orchestrations.ControlPlane.WebUI",
             "Krackend.Sagas.Orchestrations.Runtime",
             "Krackend.Sagas.Orchestrations.Runtime.Buffering.Mule",
+            "Krackend.Sagas.Orchestrations.Runtime.ButterMorph",
+            "Krackend.Sagas.Orchestrations.Runtime.Gossip.Redis",
+            "Krackend.Sagas.Orchestrations.Runtime.Messaging.Pigeon",
             "Krackend.Sagas.Orchestrations.Runtime.Storage.EntityFramework",
             "Krackend.Sagas.Orchestrations.Runtime.WebUI",
+            "Krackend.Sagas.Orchestrations.SchemaRegistry.Abstractions",
+            "Krackend.Sagas.Orchestrations.SchemaRegistry.Atlas",
             "Krackend.Sagas.Orchestrations.WebUI.Shell",
         };
 
@@ -71,6 +89,51 @@ public sealed class PackageBoundaryTests
         Assert.DoesNotContain("Krackend.Sagas.Orchestrations.Distribution", referencedAssemblies);
         Assert.DoesNotContain("Krackend.Sagas.Orchestrations.Security", referencedAssemblies);
         Assert.DoesNotContain("Krackend.Sagas.Orchestrations.Runtime.Storage.SqlServer", referencedAssemblies);
+    }
+
+    [Fact]
+    public void CorePackagesDoNotReferencePigeonMessaging()
+    {
+        var repositoryRoot = FindRepositoryRoot();
+        var restrictedProjectFiles = new[]
+        {
+            Path.Combine(repositoryRoot, "src", "Krackend.Sagas.Orchestrations.Abstractions", "Krackend.Sagas.Orchestrations.Abstractions.csproj"),
+            Path.Combine(repositoryRoot, "src", "Krackend.Sagas.Orchestrations.Client", "Krackend.Sagas.Orchestrations.Client.csproj"),
+            Path.Combine(repositoryRoot, "src", "Krackend.Sagas.Orchestrations.Contracts", "Krackend.Sagas.Orchestrations.Contracts.csproj"),
+            Path.Combine(repositoryRoot, "src", "Krackend.Sagas.Orchestrations.Runtime", "Krackend.Sagas.Orchestrations.Runtime.csproj"),
+        };
+
+        foreach (var projectFile in restrictedProjectFiles)
+        {
+            var contents = File.ReadAllText(projectFile);
+
+            Assert.DoesNotContain("Pigeon.Messaging", contents, StringComparison.Ordinal);
+        }
+    }
+
+    [Fact]
+    public void RuntimeCoreDoesNotContainDemoBusinessHardcodes()
+    {
+        var repositoryRoot = FindRepositoryRoot();
+        var runtimeRoot = Path.Combine(repositoryRoot, "src", "Krackend.Sagas.Orchestrations.Runtime");
+        var forbiddenTerms = new[]
+        {
+            "events.sales",
+            "sales.sale.created",
+            "sale-fulfillment",
+            "inventories.reserve",
+            "payments.authorize",
+        };
+
+        foreach (var sourceFile in Directory.EnumerateFiles(runtimeRoot, "*.cs", SearchOption.AllDirectories))
+        {
+            var contents = File.ReadAllText(sourceFile);
+
+            foreach (var forbiddenTerm in forbiddenTerms)
+            {
+                Assert.DoesNotContain(forbiddenTerm, contents, StringComparison.OrdinalIgnoreCase);
+            }
+        }
     }
 
     private static string FindRepositoryRoot()
